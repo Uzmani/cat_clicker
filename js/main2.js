52a6d9f52427bd72639c6e144a775ee47eb02eab
uzmani <uzmani@yahoo.com>
2015-02-21 08:26:32 -0800

    neccessary merge

--- js/main2.js
+++ js/main2.js
@@ -5,6 +5,7 @@
   this.imgURL = img;
   this.clickedCount = 0;
   this.name = name;
+  this.admin = false;
 }
   var model = {
     init: function() {
@@ -14,9 +15,16 @@
       cats.push(new cat('https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454', 'Juju and Leito'));
       cats.push(new cat('http://stylonica.com/wp-content/uploads/2014/03/Cute-Cats-cats-33440930-1280-800.jpg', 'Lil Bob'));
       cats.push(new cat('http://stylonica.com/wp-content/uploads/2014/03/cats_animals_kittens_cat_kitten_cute_desktop_1680x1050_hd-wallpaper-753974.jpeg', 'Benji'));
+      localStorage.cats = JSON.stringify(cats);
     },
     getAllCats: function() {
-      return cats;
+      return JSON.parse(localStorage.cats);
+    },
+    adminSetTrue: function() {
+      cats[controller.currentCat].admin = true;
+    },
+    adminSetFalse: function() {
+      cats[controller.currentCat].admin = false;
     }
   };
 
@@ -25,13 +33,29 @@
       model.init();
       listView.init();
       displayView.init();
+      this.updateDisplayView(this.currentCat);
+      adminView.init();
     },
     getCats: function() {
       return model.getAllCats();
     },
-    updateDisplayView: function(self) {
-      displayView.displayCat(self)
-    }
+    updateDisplayView: function(cat) {
+      // console.log(currentCat);
+      displayView.render(cat)
+    },
+    openAdminView: function(){
+      model.adminSetTrue();
+      adminView.render();
+    },
+    closeAdminview: function() {
+      model.adminSetFalse();
+      amdinView.hide();
+    },
+    updateCatInfo: function() {
+
+    },
+
+    "currentCat": 0
   };
 
   var listView = {
@@ -39,14 +63,21 @@
       var cats = controller.getCats();
       for (var i = 0; i < cats.length; i++) {
         // appending cat html to side cat list
-        var $elem = $.parseHTML("<li><a href='#'" + cats[i].name + ">" + cats[i].name + "</a></li>")[0];
-        $('.cat-list').append($elem);
+        var elem = $.parseHTML("<li><a href='#'" + cats[i].name + ">" + cats[i].name + "</a></li>")[0];
+        $('.cat-list').append(elem);
+        $(elem).on('click', (function(cat) {
+          return function() {
+            controller.currentCat = cat;
+            controller.updateDisplayView(controller.currentCat);
+          };
+        })(i));
       }
-      $('.cat-list li').on('click', function(){
-        $('.cat-list a').removeClass('active');
-        $(this).find('a').addClass('active');
-        controller.updateDisplayView($(this)); 
-      });    
+      // $('.cat-list li').on('click', function(){
+      //   $('.cat-list a').removeClass('active');
+      //   $(this).find('a').addClass('active');
+      //   console.log($(this))
+      //   controller.updateDisplayView($(this)); 
+      // });    
     } 
   };
 
@@ -67,16 +98,46 @@
       }
 
     },
-    displayCat: function(self) {
+    render: function(cat) {
+      // console.log(controller.currentCat);
+      var cats = controller.getCats();
+      var currentCat = cats[controller.currentCat]  ;
       $('.cat-display .cat-template').hide();
-      var catName = self.find('a').html();
+      var catName = currentCat.name;
       $("h2:contains(" + catName +")").closest('.cat-template').show();
     }
-
   };
+
+  var adminView = {
+    init: function() {
+      $(".admin-btn").on("click", function(e){
+        e.preventDefault();
+        controller.openAdminView();
+      })
+      $(".cancel").on("click", function(e){
+        controller.closeAdminview();
+      })
+    },
+    render: function() {
+      console.log(cats[controller.currentCat].admin);
+      if (cats[controller.currentCat].admin === true) {
+        console.log("inside if statement")
+        $("#change-form").show();
+      }
+    },
+    hide: function() {
+      $("#change-form").hide();
+    }
+  }
 
 
   controller.init();
+
+// -model will store whether admin view showing? true or false
+
+// - based on admin true or false, the view will display the form elements
+
+// - controller: opening admin view, closing view when cancel, updating the cat infomation when save
 
 
 // });
